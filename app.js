2442d8bfc0e60b296c85e07bdc13db6054aa3bd7
Zishan Ali <zishanali06@gmail.com>
2019-03-08 20:00:14 -0600

    added readme

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ let favMovie = (movie = 'The Room', name = 'world') => {
     console.log(`my name is ${name} and my favorite movie is ${movie}.`)
 }
 
+// SAME AS ABOVE
+// let favMovie = (movie = 'The Room', name = 'world') => console.log(`my name is ${name} and my favorite movie is ${movie}.`);
+
 favMovie('Run with the Wind');
