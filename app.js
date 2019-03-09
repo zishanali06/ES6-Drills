2e5ec4c39bdab730fdde0bab1c5b225c268e678f
Zishan Ali <zishanali06@gmail.com>
2019-03-08 20:45:38 -0600

    at spread syntax

--- app.js
+++ app.js
@@ -5,4 +5,30 @@ let favMovie = (movie = 'The Room', name = 'world') => {
 // SAME AS ABOVE
 // let favMovie = (movie = 'The Room', name = 'world') => console.log(`my name is ${name} and my favorite movie is ${movie}.`);
 
+// let getFirstName = (fullname) => {
+//     let splitfullname = fullname.split(' ');
+//     return console.log(splitfullname[0]);
+// }; 
+
+// let getFirstName = (fullname) => {
+//     let splitfullname = fullname.split(' ');
+//     return console.log(splitfullname[0]);
+// }; 
+let fullname1 = {
+    firstname: 'Zishan',
+    lastname: 'Ali'
+};
+
+let getFirstName = (fullname) => console.log(fullname.firstname);
+
 favMovie('Run with the Wind');
+
+getFirstName(fullname1);
+
+let lastFunc = (num1, num2) => ({
+    exp: num1^num2,
+    product: num1 * num2
+});
+
+let stats = lastFunc(4,12);
+console.log(`The expo of the numbers is ${stats.exp} and the product of the numbers is ${stats.product}`);
